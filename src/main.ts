b2c0b712cf5fd02e5087a869921624ca772d5b25
yuanliu147 <yuanliu147@qq.com>
2025-12-18 22:24:15 +0800

    feat: 父子窗口同步，子窗口视觉上位于父窗口内部

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'node:path';
 import started from 'electron-squirrel-startup';
+import { PuppeteerWindowManager } from './main/PuppeteerWindowManager';
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
@@ -10,9 +11,11 @@ if (started) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1500,
+    height: 900,
+    resizable: false, // 禁用resize
     webPreferences: {
+      webviewTag: true,
       preload: path.join(__dirname, 'preload.js'),
     },
   });
@@ -26,8 +29,25 @@ const createWindow = () => {
     );
   }
 
+  const puppeteerWindowManager = PuppeteerWindowManager.getInstance();
+  puppeteerWindowManager.createWindow(mainWindow);
+
+
+  puppeteerWindowManager.openUrl('https://www.baidu.com');
+
+
+  mainWindow.on('will-resize', (e, newBounds) => {
+    puppeteerWindowManager.updateWindowBounds(newBounds);
+  });
+
+  mainWindow.on('move', () => {
+    const [width, height] = mainWindow.getSize();
+    const { x, y } = mainWindow.getBounds();
+    puppeteerWindowManager.updateWindowBounds({ x, y, width, height });
+  })
+
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
---
+++ src/main/PuppeteerWindowManager.ts
@@ -0,0 +1,61 @@
+import { BrowserWindow } from 'electron';
+
+const ZOOM_FACTOR = 0.5;
+
+export class PuppeteerWindowManager {
+  static instance: PuppeteerWindowManager;
+  window: BrowserWindow;
+
+  static getInstance() {
+    if (!PuppeteerWindowManager.instance) {
+      PuppeteerWindowManager.instance = new PuppeteerWindowManager();
+    }
+    return PuppeteerWindowManager.instance;
+  }
+
+  createWindow(mainWindow: BrowserWindow) {
+
+    const [width, height] = mainWindow.getSize();
+    const { x, y } = mainWindow.getBounds();
+
+    this.window = new BrowserWindow({
+      frame: false,
+      transparent: true,
+      hasShadow: false,
+      resizable: false,
+      focusable: true,
+      movable: false,
+      roundedCorners: false,
+      show: true,
+      fullscreenable: false,
+      skipTaskbar: true,
+      width: width - 680,
+      height: height - 100,
+      x: x + 660,
+      y: y + 80,
+      parent: mainWindow,
+      webPreferences: {
+        zoomFactor: ZOOM_FACTOR,
+      }
+    });
+
+    this.window.webContents.setWindowOpenHandler(({ url }) => {
+      this.window.loadURL(url);
+      return { action: 'deny' };
+    })
+
+    this.window.once('ready-to-show', () => {
+      this.window.webContents.setZoomFactor(ZOOM_FACTOR);
+      this.window.moveTop();
+    })
+  }
+
+  openUrl(url: string) {
+    this.window.loadURL(url);
+  }
+
+  updateWindowBounds(bounds: { x: number, y: number, width: number, height: number }) {
+    const { x, y, width, height } = bounds;
+    this.window.setBounds({ x: x + 660, y: y + 80, width: width - 680, height: height - 100 });
+  }
+}
